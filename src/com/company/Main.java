6c79de7374e2bfc9291b778ed44dcee12a91b0b7
getconnected2010 <65646133+getconnected2010@users.noreply.github.com>
2021-08-11 22:28:11 -0400

    Hashmap

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,27 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Changes made");
+        HashMap<Integer, Integer> data= new HashMap<Integer, Integer>();
+        ArrayList<Integer> hashArr = new ArrayList<Integer>();
+        Random rand = new Random();
+
+
+        int x = rand.nextInt(10000);
+        System.out.println(x);
+
+        int y = rand.nextInt(10000);
+        System.out.println(y);
+
+        hashArr.add(data.put(x, y));
+        System.out.println(hashArr.get(0));
+
 
     }
 }
